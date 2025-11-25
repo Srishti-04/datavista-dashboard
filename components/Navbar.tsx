9639c80db44ed25b1832da513c5a92ddc122edbd
Srishti-04 <srishtijaiswal2823@gmail.com>
2025-11-26 03:28:07 +0530

    Implement shadcn Navbar design and integrate UI components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,23 @@
+import { Moon } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const Navbar = () => {
   return (
-    <div className=''> Navbar</div>
+    <nav className='p-4 flex items-center justify-between'>
+        {/*LEFT*/}
+        collapseButton
+         {/*RIGHT*/}
+         <div className='flex items-center gap-4'>
+        <Link href='/'>Dashboard</Link> 
+        <Moon/>
+        <Avatar>
+  <AvatarImage src="https://tse4.mm.bing.net/th/id/OIP.M0Uak0pyXetvVhkUU-RMOAHaEJ?pid=Api&P=0&h=180" />
+  <AvatarFallback>CN</AvatarFallback>
+</Avatar>
+        </div>
+    </nav>
   )
 }
 
