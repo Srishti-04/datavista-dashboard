c9cf9c97dd423a6a21f6b92e7ef3d2334d8ab40f
Srishti-04 <srishtijaiswal2823@gmail.com>
2025-11-26 03:48:55 +0530

    Add dropdown menu component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,15 @@
-import { Moon } from 'lucide-react'
+import { Moon, User } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const Navbar = () => {
   return (
@@ -12,10 +20,23 @@ const Navbar = () => {
          <div className='flex items-center gap-4'>
         <Link href='/'>Dashboard</Link> 
         <Moon/>
-        <Avatar>
+        
+<DropdownMenu>
+  <DropdownMenuTrigger><Avatar>
   <AvatarImage src="https://tse4.mm.bing.net/th/id/OIP.M0Uak0pyXetvVhkUU-RMOAHaEJ?pid=Api&P=0&h=180" />
   <AvatarFallback>CN</AvatarFallback>
-</Avatar>
+</Avatar></DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>
+        <User/>
+        Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
         </div>
     </nav>
   )
