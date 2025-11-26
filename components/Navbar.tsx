aaeefc1507ba9e37301793674031e7c31b3f59ac
Srishti-04 <srishtijaiswal2823@gmail.com>
2025-11-26 20:51:53 +0530

    Add theme toggle (light/dark/system) using dropdown menu and refine navbar UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
-import { LogOutIcon, Moon, Settings, User } from 'lucide-react'
+"use client"
+import { Icon, LogOutIcon, Moon, Settings, Sun, User } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
@@ -10,8 +11,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Button } from './ui/button'
+import { useTheme } from 'next-themes'
 
 const Navbar = () => {
+  const{theme,setTheme}= useTheme();
   return (
     <nav className='p-4 flex items-center justify-between'>
         {/*LEFT*/}
@@ -19,7 +23,29 @@ const Navbar = () => {
          {/*RIGHT*/}
          <div className='flex items-center gap-4'>
         <Link href='/'>Dashboard</Link> 
-        <Moon/>
+        {/*THEME MENU*/}
+        <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+        {/*USER MENU*/}
+        
         
 <DropdownMenu>
   <DropdownMenuTrigger><Avatar>
@@ -43,6 +69,22 @@ const Navbar = () => {
     
   </DropdownMenuContent>
 </DropdownMenu>
+<DropdownMenu>
+  <DropdownMenuTrigger asChild>
+    <Button variant={'outline'} size='icon'>
+      
+      <span className='sr-only'>Open Menu</span>
+    </Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>
+    
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
         </div>
     </nav>
   )
