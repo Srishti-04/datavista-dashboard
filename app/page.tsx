484ead02ac00cff9f676c7b3fe17e056854ffc58
Srishti-04 <srishtijaiswal2823@gmail.com>
2025-12-11 06:11:50 +0530

    feat: add AppBarChart component and update Homepage grid layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AppBarChart from '@/components/AppBarChart';
 import { Button } from '@/components/ui/button'
 import { CirclePlus } from "lucide-react";
 
@@ -6,7 +7,7 @@ import React from 'react'
 const Homepage = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
-      <div className=" bg-primary-foreground p-4 rounded-lg xl:col-span-2">Test</div>
+      <div className=" bg-primary-foreground p-4 rounded-lg xl:col-span-2"><AppBarChart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
