377ae2f9b246c0a0c6e51299a97ca7cd9b328a2e
Srishti-04 <srishtijaiswal2823@gmail.com>
2025-12-15 20:31:55 +0530

    Implement browser usage pie chart with tooltip and trend indicator

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AppAreaChart from '@/components/AppAreaChart';
 import AppBarChart from '@/components/AppBarChart';
+import AppPieChart from '@/components/AppPieChart';
 import { Button } from '@/components/ui/button'
 import { CirclePlus } from "lucide-react";
 
@@ -10,9 +11,9 @@ const Homepage = () => {
     <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
       <div className=" bg-primary-foreground p-4 rounded-lg xl:col-span-2"><AppBarChart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg xl:col-span-2">t</div>
+      <div className="bg-primary-foreground p-4 rounded-lg xl:col-span-2"></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><AppAreaChart/></div>
       
 
