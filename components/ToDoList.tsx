b418cfc4cddf1dbff556e766d87e5c70988ac356
Srishti-04 <srishtijaiswal2823@gmail.com>
2025-12-18 05:41:00 +0530

    feat: add date picker with calendar popover to ToDoList component

--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,14 +1,45 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import { ScrollArea } from './ui/scroll-area'
 import { Card } from './ui/card'
 import { Checkbox } from './ui/checkbox'
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover'
+import { Button } from './ui/button'
+import { CalculatorIcon,  CalendarIcon } from 'lucide-react'
+import {format} from "date-fns"
+import { Calendar } from './ui/calendar'
+
+
 
 const ToDoList = () => {
+    const [date,setDate] = useState<Date | undefined>(new Date());
+    const [open,setOpen]= useState(false);
   return (
-    <div className='text-center font-medium'>Calender
+    <div className='text-center font-medium'>
+        <h1 className='text-lg font-medium mb-4'>ToDo List</h1>
+    <Popover open={open} onOpenChange={setOpen}>
+  <PopoverTrigger asChild>
+    <Button className='w-full' >
+        <CalendarIcon/>
+        {date ? format(date,"PPP"):<span>Pick a date</span>}
+    </Button>
+  </PopoverTrigger>
+  <PopoverContent className='p-0 w-auto'> 
+
+  <Calendar
+    mode="single"
+    selected={date}
+    onSelect={(date)=>{
+        setDate(date)
+        setOpen(false)
+    }}
+   
+  />
+</PopoverContent>
+</Popover>
     {/* LIST */}
     
-    <ScrollArea className='max-h-[400px] mt-4 overflow-y-auto p-4'>
+    <ScrollArea className='max-h-[400px] mt-4 overflow-y-auto '>
         <div className='flex flex-col gap-4 p-2'>
        {/* LIST ITEM */}
 <Card className='p-4'>
